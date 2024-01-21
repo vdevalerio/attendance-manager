bc77f1cb10c0e0709178d5b7244eb73d23b24a5f
Marcos Valério <marcos@umobi.com.br>
2024-01-21 01:50:38 -0300

    Add create person page

---
+++ app/Livewire/Person/CreatePerson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Person;
+
+use Livewire\Component;
+
+class CreatePerson extends Component
+{
+    public function render()
+    {
+        return view('livewire.person.create-person');
+    }
+}
